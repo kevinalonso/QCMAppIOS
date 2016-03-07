ec3483f4c9361c418e66291baa2300ecf2627968
etudiant <etudiant@macbook-pro-de-etudiant.local>
2016-03-07 13:41:15 +0100

    Initial Commit

---
+++ QCMapp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QCMapp
+//
+//  Created by etudiant on 07/03/2016.
+//  Copyright © 2016 etudiant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ QCMapp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  QCMapp
+//
+//  Created by etudiant on 07/03/2016.
+//  Copyright © 2016 etudiant. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
